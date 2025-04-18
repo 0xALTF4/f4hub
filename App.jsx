4196722a68cd13a7482e9c86c4caa63236a8c0ba
ALTF4 <159475370+0xALTF4@users.noreply.github.com>
2025-04-18 13:17:37 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="text-white bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">f4hub is live!</h1>
+    </div>
+  )
+}
+export default App
